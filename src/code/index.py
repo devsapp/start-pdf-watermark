6604ad182bfda53aa9061be34a9043aa6d7a9122
rsonghuster <ls_huster@163.com>
2023-11-13 23:10:04 +0800

    start-pdf-watermark-v3 use fc 3.0

--- src/code/index.py
+++ src/code/index.py
@@ -11,21 +11,27 @@ from reportlab.pdfbase import pdfmetrics, ttfonts
 
 
 # support chinese
-pdfmetrics.registerFont(ttfonts.TTFont('zenhei', os.path.join(
-    "/usr/share/fonts/truetype/wqy", 'wqy-zenhei.ttc')))
-pdfmetrics.registerFont(ttfonts.TTFont('microhei', os.path.join(
-    "/usr/share/fonts/truetype/wqy", 'wqy-microhei.ttc')))
+pdfmetrics.registerFont(
+    ttfonts.TTFont(
+        "zenhei", os.path.join("/usr/share/fonts/truetype/wqy", "wqy-zenhei.ttc")
+    )
+)
+pdfmetrics.registerFont(
+    ttfonts.TTFont(
+        "microhei", os.path.join("/usr/share/fonts/truetype/wqy", "wqy-microhei.ttc")
+    )
+)
 
 
 def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
     print(pdf_file_in, pdf_file_mark, pdf_file_out)
     pdf_output = PdfWriter()
-    with open(pdf_file_in, 'rb') as input_stream:
+    with open(pdf_file_in, "rb") as input_stream:
         pdf_input = PdfReader(input_stream, strict=False)
 
         # pageNum = pdf_input.getNumPages()
         pageNum = len(pdf_input.pages)
-        pdf_watermark = PdfReader(open(pdf_file_mark, 'rb'), strict=False)
+        pdf_watermark = PdfReader(open(pdf_file_mark, "rb"), strict=False)
 
         for i in range(pageNum):
             page = pdf_input.pages[i]
@@ -33,11 +39,20 @@ def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
             page.compress_content_streams()
             pdf_output.add_page(page)
 
-        with open(pdf_file_out, 'wb') as f:
+        with open(pdf_file_out, "wb") as f:
             pdf_output.write(f)
 
 
-def _create_watermark(mark_text, pagesize=(21*cm, 29.7*cm), font="Helvetica", font_size=30, font_color=(0, 0, 0), rotate=0, opacity=1, density=(5*cm, 5*cm)):
+def _create_watermark(
+    mark_text,
+    pagesize=(21 * cm, 29.7 * cm),
+    font="Helvetica",
+    font_size=30,
+    font_color=(0, 0, 0),
+    rotate=0,
+    opacity=1,
+    density=(5 * cm, 5 * cm),
+):
     file_name = "/tmp/mark.pdf"
     c = canvas.Canvas(file_name, pagesize=pagesize)
     c.setFont(font, font_size)
@@ -48,35 +63,35 @@ def _create_watermark(mark_text, pagesize=(21*cm, 29.7*cm), font="Helvetica", fo
     c.setFillAlpha(opacity)
 
     row_gap, col_gap = density
-    colN = int(pagesize[0]/col_gap)
-    rowN = int(pagesize[1]/row_gap)
+    colN = int(pagesize[0] / col_gap)
+    rowN = int(pagesize[1] / row_gap)
     x = colN * 4
     y = rowN * 4
 
     for i in range(y):
         for j in range(x):
-            a = col_gap*(j - 2*colN)
-            b = row_gap*(i - 2*rowN)
+            a = col_gap * (j - 2 * colN)
+            b = row_gap * (i - 2 * rowN)
             c.drawString(a, b, mark_text)
 
     c.save()
 
 
 def create_watermark(evt):
-    mark_text = evt['mark_text']
+    mark_text = evt["mark_text"]
     # 1cm = 28.346456692913385， defalut is A4, (21*cm, 29.7*cm)
-    pagesize = evt.get(
-        'pagesize', [595.275590551181, 841.8897637795275])
-    font = evt.get('font', 'Helvetica')
-    font_size = evt.get('font_size', 30)
-    font_color = evt.get('font_color', (0, 0, 0))
-    rotate = evt.get('rotate', 0)
-    opacity = evt.get('opacity', 0.1)
+    pagesize = evt.get("pagesize", [595.275590551181, 841.8897637795275])
+    font = evt.get("font", "Helvetica")
+    font_size = evt.get("font_size", 30)
+    font_color = evt.get("font_color", (0, 0, 0))
+    rotate = evt.get("rotate", 0)
+    opacity = evt.get("opacity", 0.1)
     # default is (7*cm, 10*cm)
-    density = evt.get('density', [198.4251968503937, 283.46456692913387])
-    _create_watermark(mark_text, pagesize, font, font_size,
-                      font_color, rotate, opacity, density)
-    print('create_watermark success!')
+    density = evt.get("density", [198.4251968503937, 283.46456692913387])
+    _create_watermark(
+        mark_text, pagesize, font, font_size, font_color, rotate, opacity, density
+    )
+    print("create_watermark success!")
 
 
 def handler(event, context):
@@ -88,24 +103,23 @@ def handler(event, context):
     fileDir, tempfilename = os.path.split(pdf_file)
     shortname, _ = os.path.splitext(tempfilename)
     creds = context.credentials
-    auth = oss2.StsAuth(creds.access_key_id,
-                        creds.access_key_secret, creds.security_token)
-    bucket = oss2.Bucket(auth, oss_endpoint, os.environ['OSS_BUCKET'])
+    auth = oss2.StsAuth(
+        creds.access_key_id, creds.access_key_secret, creds.security_token
+    )
+    bucket = oss2.Bucket(auth, oss_endpoint, os.environ["OSS_BUCKET"])
 
-     try:
-        bucket.get_object_to_file(pdf_file, '/tmp/' + tempfilename)
+    try:
+        bucket.get_object_to_file(pdf_file, "/tmp/" + tempfilename)
     except Exception as err:
         return "get target file failed, error " + repr(err)
-        
+
     pdf_file = os.path.join(fileDir, shortname + "_out.pdf")
     create_watermark(evt)
-    local_pdf_out_file = '/tmp/' + shortname + "_out.pdf"
-    add_watermark('/tmp/' + shortname + ".pdf",
-                  '/tmp/mark.pdf', local_pdf_out_file)
-    print('pdf add_watermark success!')
+    local_pdf_out_file = "/tmp/" + shortname + "_out.pdf"
+    add_watermark("/tmp/" + shortname + ".pdf", "/tmp/mark.pdf", local_pdf_out_file)
+    print("pdf add_watermark success!")
 
-    result = bucket.put_object_from_file(
-        pdf_file, local_pdf_out_file)
+    result = bucket.put_object_from_file(pdf_file, local_pdf_out_file)
 
     subprocess.check_call(["ls", "-ll", "/tmp"])
     subprocess.check_call("rm -rf /tmp/*", shell=True)
